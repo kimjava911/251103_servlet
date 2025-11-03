e4e77d1374477dfab64cf185e3728c21be487c48
kimjava911 <aibackend4th@gmail.com>
2025-11-03 11:47:58 +0900

    step03

---
+++ src/main/java/org/example/servlet/JSPServlet.java
@@ -0,0 +1,23 @@
+package org.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/first")
+public class JSPServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 현재 경로를 유지한채로 파일만 jsp를 불러오게 할 수 있음
+        // req <- 뭔가 작업을 해줌...
+        // first라고 하는 '경로'로 들어올 땐 해당 작업 반영
+        // first.jsp <- 파일로 들어오면 해당 작업 무시.
+        // 직접 접근을 허용하는 것이 보안적으로 좋지 X <- jsp 보안적으로 처리하려고 하면 필연적으로 코드가 더러워짐.
+        // webapp -> jsp
+        req.getRequestDispatcher("/first.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/first.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>제 첫 JSP 에요</title>
+</head>
+<body>
+    <h1>첫 JSP임다</h1>
+    <p>예쁘게 봐주세요</p>
+</body>
+</html>
