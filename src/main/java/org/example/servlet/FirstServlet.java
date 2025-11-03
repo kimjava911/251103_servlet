3df93a2dbd1d1bd038ba9c55f17918e28f9d7bc8
kimjava911 <aibackend4th@gmail.com>
2025-11-03 11:11:38 +0900

    step02-1

--- src/main/java/org/example/servlet/FirstServlet.java
+++ src/main/java/org/example/servlet/FirstServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 // Servlet - Interface <- Servlet의 생명주기를 적어놓은 인터페이스
 // HttpServlet - Abstract Class <- HTTP 요청에 대해서 어느정도 대응이 되어 있는 추상 클래스
@@ -33,8 +34,25 @@ public class FirstServlet extends HttpServlet {
         System.out.println("First Servlet이 GET 요청 처리 시작");
 //        super.doGet(req, resp); // 구현이 안되어있을 경우 405를 처리하는 것
         resp.setContentType("text/html; charset=UTF-8");
-        resp.getWriter().println("반가워요!");
+//        resp.getWriter().println("반가워요!");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>제 첫 서블릿입니다!</h1>");
+        writer.println("<p>귀엽죠?</p>");
+        writer.println("<form method='post'>");
+        writer.println("<button>POST 요청</button>");
+        writer.println("</form>");
         System.out.println("First Servlet이 GET 요청 처리 종료");
+        // service로 돌아감
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("First Servlet이 POST 요청 처리 시작");
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>POST 요청을 보내셨네요</h1>");
+        writer.println("<p>왜죠?</p>");
+        System.out.println("First Servlet이 POST 요청 처리 종료");
         // service로 돌아감
     }
 
